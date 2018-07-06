9bef7987e23bb6c213f63edf40975628a5476344
Paul Asquin <paul.asquin@awabot.com>
2018-07-06 16:18:26 +0200

    Clean predict and use extern image reading

--- From_Scratch_CNN/cv-tricks/predict.py
+++ From_Scratch_CNN/cv-tricks/predict.py
@@ -1,47 +1,79 @@
+import dataset
+import train
 import tensorflow as tf
 import numpy as np
-import os, glob, cv2
+import os
 import sys, argparse
 
-# First, pass the path of the image
-dir_path = os.path.dirname(os.path.realpath(__file__))
-image_path = sys.argv[1]
-filename = dir_path + '/' + image_path
-image_size = 128
-num_channels = 3
-images = []
-# Reading the image using OpenCV
-image = cv2.imread(filename)
-# Resizing the image to our desired size and preprocessing will be done exactly as done during training
-image = cv2.resize(image, (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
-images.append(image)
-images = np.array(images, dtype=np.uint8)
-images = images.astype('float32')
-images = np.multiply(images, 1.0 / 255.0)
-# The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
-x_batch = images.reshape(1, image_size, image_size, num_channels)
+sys.path.append('../../')
+from tools import *
 
-## Let us restore the saved model 
-sess = tf.Session()
-# Step-1: Recreate the network graph. At this step only graph is created.
-saver = tf.train.import_meta_graph('dogs-cats-model.meta')
-# Step-2: Now let's load the weights saved using the restore method.
-saver.restore(sess, tf.train.latest_checkpoint('./'))
+def main():
 
-# Accessing the default graph which we have restored
-graph = tf.get_default_graph()
+    # === Open and process image ===
+    # First, pass the path of the image
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    image_path = sys.argv[1]
+    if image_path[0] != "/":
+        image_path = dir_path + '/' + image_path
+    image_size = train.IMG_SIZE
+    num_channels = train.NUM_CHANNELS
+    images = []
+    image = dataset.read_image(filename=image_path, image_size=image_size, num_channels=num_channels)
+    images.append(image)
+    images = np.array(images, dtype=np.uint8)
+    # The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
+    x_batch = images.reshape(1, image_size, image_size, num_channels)
 
-# Now, let's get hold of the op that we can be processed to get the output.
-# In the original network y_pred is the tensor that is the prediction of the network
-y_pred = graph.get_tensor_by_name("y_pred:0")
+    # === Find, open and choose a model ===
+    les_meta_path = locate_files(extension=".meta", path=os.getcwd(), dbName="meta")
+    print("Choose a model : ")
+    for i, meta_path in enumerate(les_meta_path):
+        print("\n\n" + str(i) + " : " + str(meta_path))
+        info_txt_path = str('/'.join(meta_path.split("/")[:-2]) + "/info.txt")
+        print(info_txt_path)
+        try:
+            with open(info_txt_path, 'r') as f:
+                for line in f:
+                    print("\t" + str(line.replace("\n", "")))
+            print("")
+        except FileNotFoundError:
+            print("// No info.txt \n")
+    model_num = int(input(">> "))
 
-## Let's feed the images to the input placeholders
-x = graph.get_tensor_by_name("x:0")
-y_true = graph.get_tensor_by_name("y_true:0")
-y_test_images = np.zeros((1, len(os.listdir('training_data'))))
+    try:
+        meta_path = les_meta_path[model_num]
+        model_dir_path = '/'.join(meta_path.split("/")[:-1]) + "/"
+    except IndexError or TypeError:
+        print("Wrong input")
+        return -1
 
-### Creating the feed_dict that is required to be fed to calculate y_pred 
-feed_dict_testing = {x: x_batch, y_true: y_test_images}
-result = sess.run(y_pred, feed_dict=feed_dict_testing)
-# result is of this format [probabiliy_of_rose probability_of_sunflower]
-print(result)
+    ## Let us restore the saved model
+    sess = tf.Session()
+    # Step-1: Recreate the network graph. At this step only graph is created.
+    saver = tf.train.import_meta_graph(meta_path)
+    # Step-2: Now let's load the weights saved using the restore method.
+    print("model_dir_path : " + str(model_dir_path))
+    saver.restore(sess, tf.train.latest_checkpoint(model_dir_path))
+
+    # Accessing the default graph which we have restored
+    graph = tf.get_default_graph()
+
+    # Now, let's get hold of the op that we can be processed to get the output.
+    # In the original network y_pred is the tensor that is the prediction of the network
+    y_pred = graph.get_tensor_by_name("y_pred:0")
+
+    ## Let's feed the images to the input placeholders
+    x = graph.get_tensor_by_name("x:0")
+    y_true = graph.get_tensor_by_name("y_true:0")
+    y_test_images = np.zeros((1, len(os.listdir('training_data'))))
+
+    ### Creating the feed_dict that is required to be fed to calculate y_pred
+    feed_dict_testing = {x: x_batch, y_true: y_test_images}
+    result = sess.run(y_pred, feed_dict=feed_dict_testing)
+    # result is of this format [probabiliy_of_rose probability_of_sunflower]
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
